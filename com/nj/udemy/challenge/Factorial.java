be069ba3614ff79a23906ae3e66f5c12d1a3d7bc
CoderGirlNidhi <nidhi.jain.us@gmail.com>
2023-01-22 00:01:46 -0800

    Factorial nit

--- com/nj/udemy/challenge/Factorial.java
+++ com/nj/udemy/challenge/Factorial.java
@@ -4,7 +4,7 @@ public class Factorial {
 
     public static void main(String[] args) {
         int factorial = 5;
-        System.out.println("Factorial of " + factorial+ " is " + calculateFactorialIterative(factorial));
+        System.out.println("Factorial of " + factorial + " is " + calculateFactorialIterative(factorial));
         System.out.println("Factorial of " + factorial + " is " + calcFactorialRecursive(factorial));
     }
 
