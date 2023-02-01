9cf77f0952cede9eb64a97a8ddddde4d22c75fc3
CoderGirlNidhi <nidhi.jain.us@gmail.com>
2023-01-31 17:32:37 -0800

    LCSingleNumber done

--- com/nj/leetcode/LCSingleNumber.java
+++ com/nj/leetcode/LCSingleNumber.java
@@ -8,13 +8,16 @@ public class LCSingleNumber {
         int[] nums1 = {2, 2, 1};
         int[] nums2 = {4, 1, 2, 1, 2};
         int[] nums3 = {1};
-        assert singleNumber(nums1) == 1;
-        assert singleNumber(nums2) == 4;
-        assert singleNumber(nums3) == 1;
+        System.out.println(singleNumber(nums1));
+        System.out.println(singleNumber(nums2));
+        System.out.println(singleNumber(nums3));
     }
 
     public static int singleNumber(int[] nums) {
-        // Implement this.
-        return 0;
+        int xor = 0;
+        for (int i = 0; i < nums.length; i++) {
+            xor ^= nums[i];
+        }
+        return xor;
     }
 }
